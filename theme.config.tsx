cebc6a64989a7dfc0994248b4c2750c7210c50f9
Charan Velusamy <charanquartzzz@gmail.com>
2023-01-21 13:58:55 +0530

    saved

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/charanquartz/docs',
+    link: 'https://github.com/charanquartz',
   },
   chat: {
     link: 'https://discord.com',
